f4e02f8ed6c6fbb66f96791baba8c3094e360366
Koren <kkorenn1@gmail.com>
2024-02-19 15:22:26 +0200

    All SocialNetwork Methods

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -7,6 +7,8 @@ class SocialNetwork():
     def __init__(self , name):
         self.userList: List[User] = []
         self.userNames: List[str] = []
+      #  self.userPasswords: List[str] = []
+        self.activeUsers: List[str] = []
         if type(name) == str:
             self.name = name
         else:
@@ -16,10 +18,27 @@ class SocialNetwork():
         return self.name        
     
     def sign_up(self , userName , userPass):
-        if len(userPass) > 8 or len(userPass) < 4:
-            raise Exception("invalid password")
-        elif userName in self.userName:
-            raise Exception("User already exists")
-        else:
-            self.userList.append(User(userName , userPass))
-            self.userNames.append(userName)
+        if type(userName) == str and type(userPass) == str:
+            if len(userPass) > 8 or len(userPass) < 4:
+                raise Exception("invalid password")
+            elif userName in self.userName:
+                raise Exception("User already exists")
+            else:
+                self.userList.append(User(userName , userPass))
+                self.userNames.append(userName)
+                self.userPasswords.append(userPass)
+                self.activeUsers.append(userName)
+        else: raise Exception("Invaid type of input") 
+
+
+    def log_in(self , userName , passWord):
+        if type(userName) == str and userName in self.userNames and userName not in self.activeUsers:
+            index = self.userNames.index(userName)
+            if self.userList[index].passWord == passWord:
+                self.activeUsers.append(userName)
+        else: raise Exception("Unable to log in")        
+
+    def log_out(self , userName):
+        if userName in self.activeUsers:
+            self.activeUsers.remove(userName)
+        else: raise Exception("User already logged out or User do not exist")    
