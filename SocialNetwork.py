231b77e32f9981091b337c3d9d8973b1b56e36d3
Koren <kkorenn1@gmail.com>
2024-02-19 21:31:44 +0200

    working methods

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -2,23 +2,25 @@ from typing import List
 from User import User
 
 class SocialNetwork():
+    #singleton implementation
     _instance = None
 
     def __new__(cls, name):
         if cls._instance is None:
             cls._instance = super(SocialNetwork, cls).__new__(cls)
+        return cls._instance
 
     def __init__(self , name):
-       if not hasattr(self, 'initialized'):  
-            print("INITIALIZED")  
-            self.userList: List[User] = []
-            self.userNames: List[str] = []
-            self.activeUsers: List[str] = []
-            if type(name) == str:
+        if not hasattr(self, 'initialized'):
+            if type(name) == str:  
+                self.userList: List[User] = []
+                self.userNames: List[str] = []
+                self.activeUsers: List[str] = []
                 self.name = name
+                self.initialized = True
             else:
                 raise Exception("invalid name")
-       else: raise Exception("Cant create another network")    
+        else: raise Exception("Cant create another network")    
 
     def getName(self):
         return self.name        
@@ -27,13 +29,14 @@ class SocialNetwork():
         if type(userName) == str and type(userPass) == str:
             if len(userPass) > 8 or len(userPass) < 4:
                 raise Exception("invalid password")
-            elif userName in self.userName:
+            elif userName in self.userNames:
                 raise Exception("User already exists")
             else:
-                self.userList.append(User(userName , userPass))
+                _User = User(userName , userPass)
+                self.userList.append(_User)
                 self.userNames.append(userName)
-                self.userPasswords.append(userPass)
                 self.activeUsers.append(userName)
+                return _User
         else: raise Exception("Invaid type of input") 
 
 
@@ -41,10 +44,18 @@ class SocialNetwork():
         if type(userName) == str and userName in self.userNames and userName not in self.activeUsers:
             index = self.userNames.index(userName)
             if self.userList[index].passWord == passWord:
+                for i in self.userList:
+                    if self.userList[i].name == userName:
+                        self.userList[i].logged = True
                 self.activeUsers.append(userName)
         else: raise Exception("Unable to log in")        
 
     def log_out(self , userName):
         if userName in self.activeUsers:
+            for i in self.userList:
+                if self.userList[i].name == userName:
+                    self.userList[i].logged = False
+
             self.activeUsers.remove(userName)
+
         else: raise Exception("User already logged out or User do not exist")    
--- User.py
+++ User.py
@@ -1,13 +1,14 @@
 from typing import List
-from Post import Post
+#from Post import Post
 
 class User():
     #use observer design for notifications maybe ?
     
     def __init__(self , userName , passWord):
+        logged = True
         self.userName = userName
         self.passWord = passWord
-        self.post_list: List[Post] = []
+       # self.post_list: List[Post] = []
         self.friends_list: List[str] = []
         self.notif_list = []
         self.followsMe: List[User] = []
@@ -16,64 +17,64 @@ class User():
     def follow(self , user):        
         if not isinstance(user , User):
             raise Exception("Invalid input")
-        elif  user.getName() in self.friends_list:
+        elif  user.userName in self.friends_list:
             raise Exception("Already follows")
         else: 
-            self.friends_list.append(user.getName())
+            self.friends_list.append(user.userName)
             user.followsMe.append(self)
 
 
-    def unfollow(self , user):
-        if not isinstance(user , User):
-            raise Exception("Invalid input")
-        elif user.getName() not in self.friends_list:
-            raise Exception("You do not follow this user")
-        else: 
-            for index in user.followsMe:
-                if user.followsMe[index].getName == self.getName:
-                    user.followsMe.remove(index)
-            self.friends_list.remove(user.getName)
+    # def unfollow(self , user):
+    #     if not isinstance(user , User):
+    #         raise Exception("Invalid input")
+    #     elif user.getName() not in self.friends_list:
+    #         raise Exception("You do not follow this user")
+    #     else: 
+    #         for index in user.followsMe:
+    #             if user.followsMe[index].getName == self.getName:
+    #                 user.followsMe.remove(index)
+    #         self.friends_list.remove(user.getName)
          
 
-    def getName(self):
-        return self.userName
+    # def getName(self):
+    #     return self.userName
 
 
-    def publish_post(self , type , text , price , location ):
-        if isinstance(type , str):
-            x = 1
-        else: raise Exception("not a valid input")
-        #publishes post , sends notification for followed users
-        #         
+    # def publish_post(self , type , text , price , location ):
+    #     if isinstance(type , str):
+    #         x = 1
+    #     else: raise Exception("not a valid input")
+    #     #publishes post , sends notification for followed users
+    #     #         
 
     
-    def like(self , post):
-        if isinstance(post , Post):
-            post.addLike(self)
-        else: raise Exception("Invalid input")    
+    # def like(self , post):
+    #     if isinstance(post , Post):
+    #         post.addLike(self)
+    #     else: raise Exception("Invalid input")    
 
 
-    def comment(self , post , text):
-        if isinstance(post , Post) and type(text) == str:
-            post.addComment(self , text)
-        else: raise Exception("Invalid input")
+    # def comment(self , post , text):
+    #     if isinstance(post , Post) and type(text) == str:
+    #         post.addComment(self , text)
+    #     else: raise Exception("Invalid input")
 
 
-    def notifIfUpload(self , user):
-        #gets notification if followed posted
-        random = 1
+    # def notifIfUpload(self , user):
+    #     #gets notification if followed posted
+    #     random = 1
 
 
-    def notIfCom(self , user):
-        #gets notification if someone liked or commented ur post (do not include self likes)        
-        random = 1
+    # def notIfCom(self , user):
+    #     #gets notification if someone liked or commented ur post (do not include self likes)        
+    #     random = 1
 
 
-    def getUserInfo(self):
-        #user indo
-        random = 11     
+    # def getUserInfo(self):
+    #     #user indo
+    #     random = 11     
 
 
-    def getAllNotif(self):
-        #gets all notifications from oldest to new
-        random = 1
+    # def getAllNotif(self):
+    #     #gets all notifications from oldest to new
+    #     random = 1
